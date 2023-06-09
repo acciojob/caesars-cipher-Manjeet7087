0e78ee8cabaae18fa7c26bd63affc8973885aaa4
Manjeet7087 <131547570+Manjeet7087@users.noreply.github.com>
2023-06-09 16:51:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,32 @@
 // Your Script here.
+function rot13(str) {
+  var decodedStr = '';
+
+  for (var i = 0; i < str.length; i++) {
+    var char = str[i];
+
+    if (char.match(/[A-Z]/)) {
+      // Get the character code of the current character
+      var charCode = str.charCodeAt(i);
+
+      
+      if (charCode >= 65 && charCode <= 77) {
+        char = String.fromCharCode(charCode + 13);
+      } else {
+        char = String.fromCharCode(charCode - 13);
+      }
+    }
+
+    
+    decodedStr += char;
+  }
+
+  return decodedStr;
+}
+var encodedString = 'EBG13 rknzcyr.';
+var decodedString = rot13(encodedString);
+console.log(decodedString);
+
 
 const lookup = {
   A: "N",
